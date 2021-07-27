8c8831cd857981798f69aaf96c9a4bf6a7cf42fb
reza <reza@AsusVbook.me>
2021-07-27 10:22:50 +0430

    Initial commit

---
+++ bot.py
@@ -0,0 +1,45 @@
+from telegram.ext import Updater, CommandHandler
+from telegram.ext import MessageHandler, Filters
+import logging
+
+#  -------------------- START ------------------------------------
+updater = Updater(token='1765909251:AAGX1_LCh8IxCFishKKw3G20Oyl4x5EYqMA', use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+#  -------------------- FUNCTIONS --------------------------------
+def hello(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm FPL bot, please talk to me!")
+
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+def concat(update, context):
+    txt = '-'.join(context.args)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=txt)
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+#  -------------------- HANDLERS --------------------------------
+concat_handler = CommandHandler('concat', concat)
+dispatcher.add_handler(concat_handler)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+start_handler = CommandHandler('start', hello)
+dispatcher.add_handler(start_handler)
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+#  -------------------- RUN --------------------------------
+updater.start_polling()
+updater.idle()
